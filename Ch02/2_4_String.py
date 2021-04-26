7e92cd952f91ec5a4a8e81dad13cd2e46619c5d2
zsx29 <zsx29@naver.com>
2021-04-26 17:51:42 +0900

    2021/04/26 python Ch02 문자열 곱하기  수정

--- Ch02/2_4_String.py
+++ Ch02/2_4_String.py
@@ -4,8 +4,12 @@
 내용 : 파이썬 string 예제 교재 p48
 """
 
-#문자열 더하기
+# 문자열 더하기
 str1 = "hello"
 str2 = "python"
 str3 = str1 + str2
 print("str3:", str3)
+
+# 문자열 곱하기
+name = "박재형"
+print("name * 3:", name * 3)
